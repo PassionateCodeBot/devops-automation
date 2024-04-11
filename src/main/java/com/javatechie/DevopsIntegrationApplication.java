a443826c7fd539adf69bc9c7297c6f40b529c21f
Manoj Kumar <talktokumarmanoj@gmail.com>
2024-04-11 13:35:17 +0100

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "DevOps Demo Project";
 	}
 
 	public static void main(String[] args) {
