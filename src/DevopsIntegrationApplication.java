692875a465d1dd2a666e59ba76c31c1f1c7a35a4
Manoj Kumar <talktokumarmanoj@gmail.com>
2024-04-22 17:07:10 +0100

    Add files via upload

---
+++ src/DevopsIntegrationApplication.java
@@ -0,0 +1,25 @@
+package com.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsIntegrationApplication {
+
+	@GetMapping
+	public String message(){
+		return "DevOps Demo Project";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntegrationApplication.class, args);
+
+		//SpringApplication application = new SpringApplication(MyApplication.class);
+  		// Customize application settings here
+  		//application.run(args);
+	}
+
+}
